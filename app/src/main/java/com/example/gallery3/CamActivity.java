aabc4d143658a1a9a61502908a23d41caa245bf0
ranjil123 <ranjilashrestha123.com>
2022-11-10 18:08:34 +0545

    firebase

---
+++ app/src/main/java/com/example/gallery3/CamActivity.java
@@ -0,0 +1,154 @@
+package com.example.gallery3;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+public class CamActivity extends AppCompatActivity {
+
+    ImageView img;
+    Button camera;
+    Button extract;
+    private final int Cam_Request_Code = 100;
+    StorageReference storageReference;
+    String currentPhotoPath;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cam);
+
+        img = findViewById(R.id.imageView);
+        camera = findViewById(R.id.camera);
+        extract = findViewById(R.id.extract);
+
+
+        extract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(CamActivity.this, "Extract text here", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                    Intent icam = new Intent();
+//                    icam.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+//                    startActivityForResult(icam, Cam_Request_Code);
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+
+        @Override
+        protected void onActivityResult ( int requestCode, int resultCode, @Nullable Intent data){
+            super.onActivityResult(requestCode, resultCode, data);
+
+            if (requestCode == Cam_Request_Code) {
+                if (resultCode == Activity.RESULT_OK) {
+                   File f = new File(currentPhotoPath);
+                   img.setImageURI(Uri.fromFile(f));
+                    Log.d("tag","Absolute URL of image is " + Uri.fromFile(f));
+                    //image to upload in gallery
+                        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);//scan for new image file
+                        Uri contentUri = Uri.fromFile(f);
+                        mediaScanIntent.setData(contentUri);//display in gallery
+                        this.sendBroadcast(mediaScanIntent); //inform mediaScanIntent that new file is created and that will be displayed in galler
+                        uploadFirebase(f.getName(),contentUri);
+                }
+            }
+
+        }
+
+    private void uploadFirebase(String name, Uri contentUri) {
+        storageReference  = FirebaseStorage.getInstance().getReference().child("images/" + name );
+        storageReference.putFile(contentUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Toast.makeText(CamActivity.this, "Images Uploaded", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnCanceledListener(new OnCanceledListener() {
+                    @Override
+                    public void onCanceled() {
+                        Toast.makeText(CamActivity.this, "Uploading failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "PNG_" + timeStamp + "_";
+       // File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES); //for image to appear in gallery
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".png",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.gallery3.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, Cam_Request_Code);
+            }
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/gallery3/MainActivity.java
+++ app/src/main/java/com/example/gallery3/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.gallery3;
 import static android.view.View.VISIBLE;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.ClipData;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -11,6 +12,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -23,18 +25,31 @@ import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.UUID;
 
 
 public class MainActivity extends AppCompatActivity implements RecyclerAdapter.CountOfImagesWhenRemoved{
     RecyclerView recyclerView;
     ImageView gallery;
+    ImageView camera;
     TextView text;
     private View extract;
+    RecyclerAdapter adapter;
 
     ArrayList<Uri> list;
     int Read_Permission = 0;
-
+    int PERMISSION_CODE = 1;
+    private Uri imageUri;
+    StorageReference storageReference;
 
 
     @Override
@@ -50,12 +65,21 @@ public class MainActivity extends AppCompatActivity implements RecyclerAdapter.C
 
         text = findViewById(R.id.count);
 
-
-
+        camera = findViewById(R.id.camera);
 
         handlePermission();
 
         list = new ArrayList<>();
+
+        adapter = new RecyclerAdapter(list, MainActivity.this, this);
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Intent i  = new Intent(MainActivity.this,CamActivity.class);
+                startActivity(i);
+            }
+        });
 
         gallery.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,11 +104,21 @@ public class MainActivity extends AppCompatActivity implements RecyclerAdapter.C
     }
 
     private void handlePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) ==
+                    PackageManager.PERMISSION_DENIED || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_DENIED) {
+                String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                requestPermissions(permission, PERMISSION_CODE);
+            }
+        }
+
         if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, Read_Permission);
         }
 
     }
+
 
 
     @Override
@@ -96,17 +130,27 @@ public class MainActivity extends AppCompatActivity implements RecyclerAdapter.C
                 ClipData clipData = data.getClipData();
                 if (clipData != null) {
                     for (int i = 0; i < clipData.getItemCount(); i++) {
-                        Uri imageUri = clipData.getItemAt(i).getUri();
+                        imageUri = clipData.getItemAt(i).getUri();
                         list.add(imageUri);
+                        //image will be uploaded to firebase after being picked from gallery
+                        uploadToFirebase();
                         setImage();
 
                     }
 
+                    adapter.notifyDataSetChanged();
                     text.setText("Photos:(" + list.size() + ")");
+
                 } else {
                     list.add(data.getData());
+                    uploadToFirebase();
                     setImage();
+                    adapter.notifyDataSetChanged();
+                    text.setText("Photos:(" + list.size() + ")");
                 }
+
+
+
 
             } else {
                 Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
@@ -118,27 +162,48 @@ public class MainActivity extends AppCompatActivity implements RecyclerAdapter.C
     }
 
 
+
+
     private void setImage() {
-        RecyclerAdapter adapter = new RecyclerAdapter(list, MainActivity.this, this);
         recyclerView.setAdapter(adapter);
         LinearLayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
         recyclerView.setLayoutManager(layoutManager);
 
-//          if(adapter.getItemCount()>0){
-//              extract.setVisibility(View.VISIBLE);
-//          }
-//          else if(adapter.getItemCount()==0){
-//              extract.setVisibility(View.INVISIBLE);
-//          }
-
+          if(adapter.getItemCount()>0){
+              extract.setVisibility(View.VISIBLE);
+          }
+          else{
+                  extract.setVisibility(View.INVISIBLE);
+              }
 
     }
+
+
     @Override
     public void clicked(int getSize)
     {
         text.setText("Photos:(" + list.size() + ")");
     }
 
+    private void uploadToFirebase() {
+        final String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());          //in firebase images will be stored in images folder with name of any date and time
+        String imageFileName = "PNG_" + timeStamp + "_";
+        storageReference  = FirebaseStorage.getInstance().getReference().child("images/"+imageFileName);
+        storageReference.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(MainActivity.this, "Images Uploaded", Toast.LENGTH_SHORT).show();
+            }
+        })
+                .addOnCanceledListener(new OnCanceledListener() {
+                    @Override
+                    public void onCanceled() {
+                        Toast.makeText(MainActivity.this, "Uploading failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
 
 
 }
